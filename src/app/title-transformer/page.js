9503ff3e5f8f75a1e32a4af7bf2b3b8967654612
mfnahdi <mfnahdi8@gmail.com>
2024-02-08 04:22:23 +0700

    feat: title-transformer

---
+++ src/app/title-transformer/page.js
@@ -0,0 +1,14 @@
+import { Box, Center } from "@chakra-ui/react";
+import TitleTransformer from "./components/TitleTransformer";
+
+const page = () => {
+  return (
+    <Box>
+      <Center>
+        <TitleTransformer />
+      </Center>
+    </Box>
+  );
+};
+
+export default page;
